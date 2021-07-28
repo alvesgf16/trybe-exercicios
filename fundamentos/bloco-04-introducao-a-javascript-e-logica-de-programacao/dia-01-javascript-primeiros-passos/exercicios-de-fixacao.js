c1dd8f27d5607a027772175cc61d4b8add63e1d3
alvesgf16 <alvesgabriel1994@gmail.com>
2021-07-28 15:43:18 -0300

    Adiciona exercicio 2.2

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios-de-fixacao.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios-de-fixacao.js
@@ -1,14 +1,9 @@
-let patientId = 50;
-let isEnrolled = true;
-const patientInfo = {
-  firstName: 'Ana',
-  lastName: 'Santos',
-};
-const patientEmail = 'ana@email.com';
+let base = 5;
+let altura = 8;
+let area = base * altura;
 
-console.log(typeof patientAge);
-console.log(typeof patientId);
+console.log(area);
 
-patientId = '50';
+let perimetro = base + altura + base + altura;
 
-console.log(typeof patientId);
+console.log(perimetro);
