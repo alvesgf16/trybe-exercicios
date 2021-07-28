b14a924c60438767d99f6c8cdafc804744ad1e07
alvesgf16 <alvesgabriel1994@gmail.com>
2021-07-28 15:36:00 -0300

    Adiciona exercicio 1

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicios-de-fixacao.js
@@ -0,0 +1,11 @@
+const myName = "Gabriel";
+const birthCity = "Aracaju";
+let birthYear = 1994;
+
+console.log(myName, birthCity, birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+birthCity = "Capela";
