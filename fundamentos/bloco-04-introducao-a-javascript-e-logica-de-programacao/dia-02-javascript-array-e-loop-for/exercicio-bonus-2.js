86933cab05e1c85855eeafcc99bb7d1aa2de8930
alvesgf16 <alvesgabriel1994@gmail.com>
2021-07-29 18:44:55 -0300

    Corrige exercicios bonus 1 e 2

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicio-bonus-2.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicio-bonus-2.js
@@ -1,5 +1,5 @@
 //Exercício bônus 2: Ordene o array numbers em ordem decrescente e imprima seus valores;
-let numbers = [45, 27, 57, 1330, 560, 800, 200, 70, 945, 54];
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 for (let index = 1; index < numbers.length; index += 1) {
   for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
