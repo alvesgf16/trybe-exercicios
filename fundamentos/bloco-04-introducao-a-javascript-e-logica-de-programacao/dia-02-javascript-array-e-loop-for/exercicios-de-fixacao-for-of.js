bd83d49ffc5ea16c7652b7195be04c348c6e796c
alvesgf16 <alvesgabriel1994@gmail.com>
2021-07-29 15:46:44 -0300

    Adiciona exercicios de fixaçao de loop for/of

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicios-de-fixacao-for-of.js
@@ -0,0 +1,25 @@
+//Revisão do assunto
+// let numeros = [1,2,3,4,5];
+// for(let numero of numeros) {
+//   console.log(numero);
+// }
+
+// let word = 'Hello';
+// for (let letter of word) {
+//   console.log(letter);
+// }
+
+// let arrOfNumbers = [10, 20, 30];
+// for (let sum of arrOfNumbers) {
+//   sum += 1;
+//   console.log(sum);
+// }
+
+// console.log(arrOfNumbers);
+
+//Exercício de fixação
+let names = ['João','Maria','Antônio','Margarida'];
+
+for(let name of names) {
+    console.log(name);
+}
