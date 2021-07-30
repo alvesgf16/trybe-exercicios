3e308bc1d41ed09891826ec0a546f22f9065d3b1
alvesgf16 <alvesgabriel1994@gmail.com>
2021-07-30 18:10:34 -0300

    Adiciona exercicio 4

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicio-3.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicio-3.js
@@ -4,7 +4,6 @@ let str = "*"
 let blank = " "
 
 for (i = 1; i <= n; i += 1) {
-
     console.log(blank.repeat(n - numeroDeAstericosNaLinha) + str.repeat(numeroDeAstericosNaLinha));
     numeroDeAstericosNaLinha += 1
 }
---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicio-4.js
@@ -0,0 +1,9 @@
+let n = 5;
+let numeroDeAstericosNaLinha = 1;
+let str = "*"
+let blank = " "
+
+for (i = 1; i <= n; i += 2) {
+    console.log(blank.repeat((n - numeroDeAstericosNaLinha)/2) + str.repeat(numeroDeAstericosNaLinha));
+    numeroDeAstericosNaLinha += 2
+}
