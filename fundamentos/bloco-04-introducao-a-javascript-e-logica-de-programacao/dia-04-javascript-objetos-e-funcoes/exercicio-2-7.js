3e3e3d331c9a0ccc195728a5ba0ca15dcdb86be5
alvesgf16 <alvesgabriel1994@gmail.com>
2021-08-03 13:43:51 -0300

    Adiciona exercicio 2.7

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercicio-2-7.js
@@ -0,0 +1,23 @@
+// 7 - Crie uma função que receba uma string word e outra string ending . Verifique se a string ending é o final da string word . Considere que a string ending sempre será menor que a string word .
+// Valor de teste: 'trybe' e 'be'
+// Valor esperado no retorno da função: true
+// verificaFimPalavra('trybe', 'be') ;
+// Retorno esperado: true
+// verificaFimPalavra('joaofernando', 'fernan') ;
+// Retorno esperado: false
+
+function verificaFimPalavra(word, ending) {
+    let wordArray = word.split("");
+    let endingArray = ending.split("");
+    let isEnding = true;
+
+    for (index = 0; index < endingArray.length; index += 1) {
+        if (wordArray[index + wordArray.length - endingArray.length] != endingArray[index]) {
+            isEnding = false
+        }
+    }
+
+    return console.log(isEnding);
+}
+
+verificaFimPalavra("joaofernando", "nando");
