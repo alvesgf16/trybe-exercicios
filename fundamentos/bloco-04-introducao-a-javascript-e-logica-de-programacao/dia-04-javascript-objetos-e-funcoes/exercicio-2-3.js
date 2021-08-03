718ee857a5cdb02c7fa356d4a2c18c9698a48908
alvesgf16 <alvesgabriel1994@gmail.com>
2021-08-03 00:24:41 -0300

    Adiciona exercicio 2.3

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercicio-2-3.js
@@ -0,0 +1,16 @@
+// 3 - Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
+// Array de teste: [2, 4, 6, 7, 10, 0, -3]; .
+// Valor esperado no retorno da função: 6 .
+
+function returnSmallestIndex(array) {
+    let smallestNumber = array[0];
+    for(let number of array) {
+        if (number < smallestNumber) {
+            smallestNumber = number;
+        }
+    }
+
+    return array.indexOf(smallestNumber);
+}
+
+console.log(returnSmallestIndex([2, 4, 6, 7, 10, 0, -3]));
