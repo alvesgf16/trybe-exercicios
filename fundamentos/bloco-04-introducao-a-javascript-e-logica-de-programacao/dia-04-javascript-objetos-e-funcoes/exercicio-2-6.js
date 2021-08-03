8c0db8e7282c4d753a4c3a4de4d68bbc61b0f9cf
alvesgf16 <alvesgabriel1994@gmail.com>
2021-08-03 13:24:23 -0300

    Adiciona exercicio 2.6

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercicio-2-6.js
@@ -0,0 +1,14 @@
+// 6 - Crie uma função que receba um número inteiro N e retorne o somatório de todos os números de 1 até N.
+// Valor de teste: N = 5 .
+// Valor esperado no retorno da função: 1+2+3+4+5 = 15 .
+
+function sumUntilNumber(finalNumber) {
+    let sum = 0;
+    for (number = 0; number <= finalNumber; number += 1) {
+        sum += number;
+    }
+
+    return console.log(sum);
+}
+
+sumUntilNumber(10);
