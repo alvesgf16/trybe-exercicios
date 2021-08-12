60a65250e224ba75c1a72f61fc563b0351278405
alvesgf16 <alvesgabriel1994@gmail.com>
2021-08-11 22:42:10 -0300

    Cria botao de sextous

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/exercicios/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/exercicios/script.js
@@ -88,7 +88,24 @@ function toggleHolidays() {
 
         holidaysOn = true;
     }
-
 }
+
+/*
+Exercício 4:
+Implemente uma função que receba como parâmetro a string "Sexta-feira" e crie dinamicamente um botão com o nome "Sexta-feira".
+Adicione a este botão o ID "btn-friday" .
+Adicione este botão como filho/filha da tag <div> com classe "buttons-container" .
+*/
+
+function createFridayButton(string) {
+    let fridayButton = document.createElement('button');
+    fridayButton.innerText = string;
+    fridayButton.id = 'btn-friday';
+    buttonsContainer.appendChild(fridayButton);
+
+    // fridayButton.addEventListener('click', toggleFridays);
+}
+
+createFridayButton('Sexta-feira');
 
 
