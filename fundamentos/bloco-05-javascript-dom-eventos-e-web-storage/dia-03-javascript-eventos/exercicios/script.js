de662a7b75a4a3ba26b6d0efe5f361a648e98321
alvesgf16 <alvesgabriel1994@gmail.com>
2021-08-12 09:14:54 -0300

    Cria funçao pra adicionar legenda a tarefa

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/exercicios/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-03-javascript-eventos/exercicios/script.js
@@ -153,11 +153,27 @@ Exercício 7:
 Implemente uma função que adiciona uma tarefa personalizada ao calendário. A função deve receber como parâmetro a string com o nome da tarefa (ex: "cozinhar") e criar dinamicamente um elemento com a tag <span> contendo a tarefa.
 O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks" .
 */
+let taskList = document.querySelector('.my-tasks');
+
 function addTask(taskStr) {
     let task = document.createElement('span');
-    let taskList = document.querySelector('.my-tasks');
     task.innerText = taskStr;
     taskList.appendChild(task);
 }
 
 addTask('Terminar exercícios do dia');
+
+/*
+Exercício 8:
+Implemente uma função que adiciona uma legenda com cor para a tarefa criada no exercício anterior. Esta função deverá receber como parâmetro uma string ("cor") e criar dinamicamente um elemento de tag <div> com a classe task .
+O parâmetro cor deverá ser utilizado como cor de fundo da <div> criada.
+O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks" .
+*/
+function addLabel(colorStr) {
+    let label = document.createElement('div');
+    label.className = 'task';
+    label.style.backgroundColor = colorStr;
+    taskList.appendChild(label);
+}
+
+addLabel('red');
