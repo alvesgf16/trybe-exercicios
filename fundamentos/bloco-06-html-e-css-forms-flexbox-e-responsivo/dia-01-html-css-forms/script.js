17e680bf0e1f4095f5f00824c2cd72b45b586b2f
alvesgf16 <alvesgabriel1994@gmail.com>
2021-08-19 16:13:47 -0300

    Adiciona verificaçoes de datas

--- fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-css-forms/script.js
+++ fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-css-forms/script.js
@@ -11,3 +11,54 @@ for (let index = 0; index < states.length; index += 1) {
 
   stateSelector.appendChild(stateOption);
 }
+
+const startDate = document.getElementById('start-date');
+const submitButton = document.getElementById('submit-resume');
+
+function checkDateContents() {
+  const dateElements = startDate.value.split('/');
+
+  for (let index = 0; index < dateElements.length; index += 1) {
+    if (isNaN(dateElements[index])){
+      alert('Data inválida');
+    }
+  }
+}
+
+function checkDayRange() {
+  const dateElements = startDate.value.split('/');
+  const day = parseInt(dateElements[0]);
+
+  if (!(day > 0 && day <= 31)) {
+    alert('Dia inválido');
+  }
+}
+
+function checkMonthRange() {
+  const dateElements = startDate.value.split('/');
+  const month = parseInt(dateElements[1]);
+  
+  if(!(month > 0 && month <= 12)) {
+    alert('Mês inválido');
+  }
+}
+
+function checkYearRange() {
+  const dateElements = startDate.value.split('/');
+  const year = parseInt(dateElements[2]);
+  
+  if (!(year > 0)) {
+    alert('Ano inválido');
+  }
+}
+
+function validateDate() {
+  checkDateContents()
+  checkDayRange();
+  checkMonthRange();
+  checkYearRange();
+}
+
+submitButton.addEventListener('click', validateDate);
+
+
