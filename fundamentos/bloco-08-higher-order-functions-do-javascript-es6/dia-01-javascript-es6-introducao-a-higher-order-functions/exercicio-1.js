5b64cb1d23c8280ecc381c6cb48c26709647951d
alvesgf16 <alvesgabriel1994@gmail.com>
2021-09-02 18:28:57 -0300

    Nomeia funçao inicial

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/exercicio-1.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/exercicio-1.js
@@ -1,10 +1,10 @@
-const func = nomeCompleto => ({ nomeCompleto, email: `${nomeCompleto}@trybe.com` });
+const addEmployee = nomeCompleto => ({ nomeCompleto, email: `${nomeCompleto}@trybe.com` });
 
 const newEmployees = () => {
   const employees = {
-    id1: func('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
-    id2: func('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
-    id3: func('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+    id1: createEmployee('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: createEmployee('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: createEmployee('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
   }
   return employees;
 };
