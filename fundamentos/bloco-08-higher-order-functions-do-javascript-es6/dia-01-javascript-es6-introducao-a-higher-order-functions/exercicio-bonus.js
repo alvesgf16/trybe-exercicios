15b91e315d7b8b3205da3cd0075fd56757946159
alvesgf16 <alvesgabriel1994@gmail.com>
2021-09-03 10:09:21 -0300

    Cria funções de cálculo de dano

---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/exercicio-bonus.js
@@ -0,0 +1,35 @@
+const mage = {
+  healthPoints: 130,
+  intelligence: 45,
+  mana: 125,
+  damage: undefined,
+};
+
+const warrior = {
+  healthPoints: 200,
+  strength: 30,
+  weaponDmg: 2,
+  damage: undefined,
+};
+
+const dragon = {
+  healthPoints: 350,
+  strength: 50,
+  damage: undefined,
+};
+
+const battleMembers = { mage, warrior, dragon };
+
+// Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const dragonAttack = () => getRandomIntInclusive(15, dragon.strength);
+const warriorAttack = () => getRandomIntInclusive(warrior.strength, warrior.weaponDmg * warrior.strength);
+const mageAttack = () => ({
+  damage: mage.mana >= 15 ? getRandomIntInclusive(mage.intelligence, mage.intelligence * 2) : 0,
+  manaConsumption: 15,
+  });
