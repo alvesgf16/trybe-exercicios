f6ccef3e020c38a592157d5d8b906e13310a9870
alvesgf16 <alvesgabriel1994@gmail.com>
2021-09-04 13:17:00 -0300

    Adiciona soluçao do exercício 5

--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercise5.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercise5.js
@@ -63,4 +63,10 @@ const books = [
   },
 ];
 
-// Adicione o código do exercício aqui:
+const expectedResult = false;
+
+function everyoneWasBornOnSecXX() {
+  return books.every(book => book.author.birthYear >= 1901 && book.author.birthYear < 2001);
+}
+
+assert.strictEqual(everyoneWasBornOnSecXX(), expectedResult);
