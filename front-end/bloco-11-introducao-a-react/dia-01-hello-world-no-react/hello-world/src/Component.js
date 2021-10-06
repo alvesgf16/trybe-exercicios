c7f38de0a8ad2a4fd5b486bbdf03d52958ff8ce7
alvesgf16 <alvesgabriel1994@gmail.com>
2021-10-06 19:39:32 -0300

    Adiciona exercicios do dia 11.1

---
+++ front-end/bloco-11-introducao-a-react/dia-01-hello-world-no-react/hello-world/src/Component.js
@@ -0,0 +1,8 @@
+function Component() {
+  const myName = <h1>Gabriel Alves</h1>
+  const summary = <p>Sou um ex-engenheiro que decidiu se aventurar pelo mundo da programação.</p>
+
+  return (<div>{myName}{summary}</div>);
+}
+
+export default Component;
---
+++ front-end/bloco-11-introducao-a-react/dia-01-hello-world-no-react/lessons-learned/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from './Header.jsx'
+import Content from './Content';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <body>
+      <header><Header /></header>
+      <main><Content /></main>
+      <footer><Footer /></footer>
+    </body>
+  );
+}
+
+export default App;
