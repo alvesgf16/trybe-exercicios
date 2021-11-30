d9dba66564284f28bd67ceb42e41caf710b78bdf
alvesgf16 <alvesgabriel1994@gmail.com>
2021-11-30 15:39:19 -0300

    Diminui duplicação de código

--- fundamentos/bloco-09-javascript-assincrono-e-promises/dia-03-jest-testes-assincronos/exercicios-de-fixacao/findAnimalsByType.test.js
+++ fundamentos/bloco-09-javascript-assincrono-e-promises/dia-03-jest-testes-assincronos/exercicios-de-fixacao/findAnimalsByType.test.js
@@ -1,22 +1,4 @@
-const Animals = [
-  { name: 'Dorminhoco', age: 1, type: 'Dog' },
-  { name: 'Soneca', age: 2, type: 'Dog' },
-  { name: 'Preguiça', age: 5, type: 'Cat' },
-];
-
-const findAnimalsByType = (type) => (
-  new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const arrayAnimals = Animals.filter((animal) => animal.type === type);
-
-      if (arrayAnimals.length !== 0) {
-        return resolve(arrayAnimals);
-      }
-
-      return reject(new Error('Não possui esse tipo de animal.'));
-    }, 100);
-  })
-);
+const { findAnimalsByType } = require('./findAnimalsByType');
 
 describe('Quando o tipo do animal existe', () => {
   test('Retorne a lista de animais', () => (
@@ -32,6 +14,6 @@ describe('Quando o tipo do animal não existe', () => {
     expect.assertions(1);
     return findAnimalsByType('Lion').catch((error) => (
       expect(error.message).toMatch('Não possui esse tipo de animal.')
-    ))
-    });
+    ));
+  });
 });
--- fundamentos/bloco-09-javascript-assincrono-e-promises/dia-03-jest-testes-assincronos/exercicios-de-fixacao/findAnimalsByTypeResolvesRejects.test.js
+++ fundamentos/bloco-09-javascript-assincrono-e-promises/dia-03-jest-testes-assincronos/exercicios-de-fixacao/findAnimalsByTypeResolvesRejects.test.js
@@ -1,22 +1,4 @@
-const Animals = [
-  { name: 'Dorminhoco', age: 1, type: 'Dog' },
-  { name: 'Soneca', age: 2, type: 'Dog' },
-  { name: 'Preguiça', age: 5, type: 'Cat' },
-];
-
-const getListAnimals = (type) => (
-  new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const arrayAnimals = Animals.filter((animal) => animal.type === type);
-
-      if (arrayAnimals.length !== 0) {
-        return resolve(arrayAnimals);
-      }
-
-      return reject(new Error('Não possui esse tipo de animal.'));
-    }, 100);
-  })
-);
+const { findAnimalsByType: getListAnimals } = require('./findAnimalsByType');
 
 describe('Testando promise - findAnimalsByType', () => {
   describe('Quando o tipo do animal existe', () => {
