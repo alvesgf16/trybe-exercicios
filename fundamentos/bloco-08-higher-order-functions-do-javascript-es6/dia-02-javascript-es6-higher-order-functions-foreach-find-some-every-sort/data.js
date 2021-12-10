60679de5849858f2a0326677946e8235745740ee
alvesgf16 <alvesgabriel1994@gmail.com>
2021-12-10 17:30:37 -0300

    Continua revisão de códigos antigos

---
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/data.js
@@ -0,0 +1,7 @@
+module.exports = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
--- fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercises-sort.js
+++ fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-02-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercises-sort.js
@@ -1,10 +1,4 @@
-const people = [
-  { name: 'Mateus', age: 18 },
-  { name: 'José', age: 16 },
-  { name: 'Ana', age: 23 },
-  { name: 'Cláudia', age: 20 },
-  { name: 'Bruna', age: 19 },
-];
+const people = require('./data');
 
 people.sort((a, b) => b.age - a.age);
 
