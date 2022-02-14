d2b2a39530442dad52874b47727fe924a1f42a73
alvesgf16 <alvesgabriel1994@gmail.com>
2022-02-14 20:45:28 -0300

    feat: exercises of day 22.3

---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-03-mocha-chai-e-sinon-testes-de-back-end-com-node-js/exercises/writeToFile/test.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const sinon = require('sinon');
+const { expect } = require('chai');
+
+const writeToFile = require('./');
+
+const CONTEUDO_DO_ARQUIVO = 'VQV com TDD';
+
+describe('writeToFile', () => {
+  describe('')
+})
