712c844688410b642f3c94612c7c5adfa51b7941
Gabriel Alves <81915403+alvesgf16@users.noreply.github.com>
2022-02-22 12:55:55 -0300

    Delete back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/dias-01-e-02-arquitetura-dde-software-camadas-de-model-service-e-controller/model-example directory

--- back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/dias-01-e-02-arquitetura-dde-software-camadas-de-model-service-e-controller/model-example/models/connection.js
+++
@@ -1,9 +0,0 @@
-const mysql = require('mysql2/promise');
-
-const connection = mysql.createPool({
-    host: 'localhost',
-    user: 'alvesgf16',
-    password: 'D@t@b@53',
-    database: 'model_example' });
-
-module.exports = connection;
--- back-end/bloco-23-node-js-camada-de-servico-e-arquitetura-rest-e-restful/dias-01-e-02-arquitetura-dde-software-camadas-de-model-service-e-controller/model-example/models/getAll.js
+++
@@ -1,8 +0,0 @@
-const connection = require('./connection');
-
-module.exports = async (columns, table, serialize) => {
-  const query = `SELECT ${columns.join(', ')} FROM ${table};`;
-  const [result] = await connection.execute(query);
-  
-  return result.map(serialize);
-};
