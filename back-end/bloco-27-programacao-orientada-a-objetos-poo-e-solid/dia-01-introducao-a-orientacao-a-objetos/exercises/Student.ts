61c5edb00c1e55b0eb0f1bfb481473a9536a6923
alvesgf16 <alvesgabriel1994@gmail.com>
2022-04-04 18:57:31 -0300

    feat: create student class

---
+++ back-end/bloco-27-programacao-orientada-a-objetos-poo-e-solid/dia-01-introducao-a-orientacao-a-objetos/exercises/Student.ts
@@ -0,0 +1,21 @@
+export default class Student {
+  private _registrationId: number;
+
+  private _name: string;
+
+  private _testGrades: number[];
+
+  private _projectGrades: number[];
+
+  constructor(
+    r: number,
+    n: string,
+    tG: number[],
+    pG: number[],
+  ) {
+    this._registrationId = r;
+    this._name = n;
+    this._testGrades = tG;
+    this._projectGrades = pG;
+  }
+}
