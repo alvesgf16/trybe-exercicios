cc2fce5a0f6cc458217b5846ad8925eb43428f0c
alvesgf16 <alvesgabriel1994@gmail.com>
2022-05-23 18:40:06 -0300

    refactor: implement generic model

--- back-end/bloco-30-mongodb-com-nodejs-e-poo/dia-01-mongodb-e-arquitetura-msc/Bookshop/src/database/models/BookModel.ts
+++ back-end/bloco-30-mongodb-com-nodejs-e-poo/dia-01-mongodb-e-arquitetura-msc/Bookshop/src/database/models/BookModel.ts
@@ -1,40 +1,9 @@
-// models/BookModel.ts
-
-import { model as createModel } from 'mongoose';
 import IBook from '../../interfaces/IBook';
 import { BookSchema } from '../schemas/BookSchema';
+import Model from './Model';
 
-class BookModel {
-  constructor(private bookModel = createModel<IBook>('books', BookSchema)) {}
-
-  public async getBooks(): Promise<IBook[]> {
-    const books = await this.bookModel.find();
-    return books;
-  }
-
-  public async createBook(bookData: object): Promise<IBook> {
-    const book = await this.bookModel.create(bookData);
-    return book;
-  }
-
-  public async getBook(id: string): Promise<IBook | null> {
-    const book = await this.bookModel.findOne({ _id: id });
-    return book;
-  }
-
-  public async editBook(id: string, bookData: object): Promise<IBook | null> {
-    const book = await this.bookModel.findOneAndUpdate(
-      { _id: id },
-      { ...bookData },
-      { new: true },
-    );
-    return book;
-  }
-
-  public async deleteBook(id: string): Promise<IBook | null> {
-    const book = await this.bookModel.findOneAndDelete({ _id: id });
-    return book;
+export default class BookModel extends Model<IBook> {
+  constructor() {
+    super('books', BookSchema);
   }
 }
-
-export default BookModel;
