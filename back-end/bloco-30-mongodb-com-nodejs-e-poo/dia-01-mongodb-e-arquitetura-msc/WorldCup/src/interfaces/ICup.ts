667e1cef7edfb2e3e40e940af32498d4af76fe02
alvesgf16 <alvesgabriel1994@gmail.com>
2022-05-23 20:36:06 -0300

    feat: mongoose schema

---
+++ back-end/bloco-30-mongodb-com-nodejs-e-poo/dia-01-mongodb-e-arquitetura-msc/WorldCup/src/interfaces/ICup.ts
@@ -0,0 +1,11 @@
+export default interface ICup {
+  year: number;
+  hostCountry: string;
+  champions: string;
+  runnerUp: string;
+  editionGoals: number;
+  editionStrikers: string[];
+  bestPlayer: string;
+  bestGoalkeeper?: string;
+  bestYoungPlayer: string;
+}
