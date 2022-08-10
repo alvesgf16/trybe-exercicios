7e863d77ce7a2fb82cf6b31a08667fe10942d567
alvesgf16 <alvesgabriel1994@gmail.com>
2022-08-10 16:16:47 -0300

    feat: instantiate and populate HashMap class
    To check if the code is working, the name of the person of id_num = 23
    is printed on the screen.

--- ciencia-da-computacao/bloco-38-estrutura-de-dados-ii-hashmaps-e-sets/dia-01-hashmap-e-dict/main.py
+++ ciencia-da-computacao/bloco-38-estrutura-de-dados-ii-hashmaps-e-sets/dia-01-hashmap-e-dict/main.py
@@ -1,6 +1,13 @@
 from Employee import Employee
-# from HashMap import HashMap
+from HashMap import HashMap
 
 employees = [(14, "name1"), (23, "name2"), (10, "name3"), (9, "name4")]
 
 employee_objects = [Employee(id_num, name) for (id_num, name) in employees]
+
+employee_hash_map = HashMap()
+
+for employee in employee_objects:
+    employee_hash_map.insert(employee)
+
+print(employee_hash_map.get_value(23))
