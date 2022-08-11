f68fa4b00ad4e5bc982f4fd490495ff8b1fb7edb
alvesgf16 <alvesgabriel1994@gmail.com>
2022-08-11 14:36:52 -0300

    refactor: implement solution with built-in dict

--- ciencia-da-computacao/bloco-38-estrutura-de-dados-ii-hashmaps-e-sets/dia-01-hashmap-e-dict/main.py
+++ ciencia-da-computacao/bloco-38-estrutura-de-dados-ii-hashmaps-e-sets/dia-01-hashmap-e-dict/main.py
@@ -1,17 +1,41 @@
 from Employee import Employee
-from HashMap import HashMap
+# from HashMap import HashMap
+
 
 employees = [(14, "name1"), (23, "name2"), (10, "name3"), (9, "name4")]
-
 employee_objects = [Employee(id_num, name) for (id_num, name) in employees]
 
-employee_hash_map = HashMap()
+
+#   employee_registry = HashMap()
+
+# Instanciando a classe Dict
+employee_registry = {}
+
+
+# Inserindo dados
+
+#   for employee in employee_objects:
+#       employee_registry.insert(employee)
+
+#   objeto[chave]         = valor
+#   employee_registry[14] = 'name1'
+#   employee_registry[23] = 'name2'
+#   employee_registry[10] = 'name3'
+#   employee_registry[9]  = 'name4'
 
 for employee in employee_objects:
-    employee_hash_map.insert(employee)
+    employee_registry[employee.id_num] = employee.name
 
-print(employee_hash_map.get_value(23))
+# print(employee_registry.get_value(23))
+# print(employee_registry.get_value(10))
+print(employee_registry)
 
-print(employee_hash_map.get_value(10))
-employee_hash_map.update_value(10, "name30")
-print(employee_hash_map.get_value(10))
+
+# Alterando o nome do id 10
+
+#   employee_registry.update_value(10, "name30")
+
+# objeto[chave]       = novo_valor
+employee_registry[10] = 'name30'
+
+print(f"Novo valor do id 10, após a atualização: {employee_registry[10]}")
