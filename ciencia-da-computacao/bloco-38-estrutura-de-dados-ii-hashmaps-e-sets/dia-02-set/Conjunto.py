c4e0258e967830a31cf32651325afb01278f2343
alvesgf16 <alvesgabriel1994@gmail.com>
2022-08-11 20:35:23 -0300

    feat: print Conjunto

--- ciencia-da-computacao/bloco-38-estrutura-de-dados-ii-hashmaps-e-sets/dia-02-set/Conjunto.py
+++ ciencia-da-computacao/bloco-38-estrutura-de-dados-ii-hashmaps-e-sets/dia-02-set/Conjunto.py
@@ -2,5 +2,10 @@ class Conjunto:
     def __init__(self):
         self.conjunto = [False for _ in range(1001)]
 
+    def __str__(self):
+        return str(
+            [index for index, value in enumerate(self.conjunto) if value]
+        )
+
     def add(self, item):
         self.conjunto[item] = True
--- ciencia-da-computacao/bloco-38-estrutura-de-dados-ii-hashmaps-e-sets/dia-02-set/main.py
+++ ciencia-da-computacao/bloco-38-estrutura-de-dados-ii-hashmaps-e-sets/dia-02-set/main.py
@@ -5,3 +5,5 @@ conjunto = Conjunto()
 
 for number in [0, 10, 100, 1000]:
     conjunto.add(number)
+
+print(conjunto)
